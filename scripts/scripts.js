db46e686f21fdc4feace00e1d8bb782956a2136d
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-03-24 19:07:33 -0700

    sticky navbar

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+/*** Javascript for we functionalities ***/
+
+window.onscroll = function() {
+	myFunction()
+};
+
+var navbar = document.getElementsByClassName("separator");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
+}
