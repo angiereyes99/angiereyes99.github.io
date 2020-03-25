a6f9cacf09d5a0edbfe350263177670a1f4b501d
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-03-24 19:19:36 -0700

    sticky navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,7 +8,5 @@ var sticky = navbar.offsetTop;
 function myFunction() {
 	if (window.pageYOffset >= sticky) {
 		navbar.classList.add("sticky");
-	} else {
-		navbar.classList.remove("sticky");
 	}
 }
