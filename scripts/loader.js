ae07dfce55bc296f04c66a52092b2f1275d7c326
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-04-05 00:32:47 -0700

    loader

---
+++ scripts/loader.js
@@ -0,0 +1,10 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementsByClassName("box").style.display = "block";
+}
