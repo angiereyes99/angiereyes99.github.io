8f407b4744b193b01d33cb432ffe450cae321b5f
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-04-07 19:42:40 -0700

    more effects

--- scripts/loader.js
+++ scripts/loader.js
@@ -17,6 +17,7 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       if (objectBottom < windowBottom) { 
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
       } else { 
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
       }
