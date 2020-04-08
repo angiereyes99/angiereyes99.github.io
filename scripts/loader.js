d7e19806befbf5f3ccb296158edcb0b40bde373e
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-04-07 17:18:35 -0700

    more effects

--- scripts/loader.js
+++ scripts/loader.js
@@ -8,3 +8,20 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("box").style.display = "flex";
 }
+
+(document).ready(function() {
+  
+  $(window).scroll( function(){
+      $('.exp-box').each( function(i){
+          
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},500);
+                  
+          } 
+      });
+  });
+});
