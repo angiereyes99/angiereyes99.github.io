2605a34097dccd95db44077fc8d6ebd3e4ff0351
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-04-07 17:24:06 -0700

    more effects

--- scripts/loader.js
+++ scripts/loader.js
@@ -25,3 +25,20 @@ $(document).ready(function() {
       });
   });
 });
+
+$(document).ready(function() {
+  
+  $(window).scroll( function(){
+      $('.project-block').each( function(i){
+          
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},500);
+                  
+          } 
+      });
+  });
+});
