a48c636d6f00d4b5ddee7cbc774847b1ad55bf8b
Angelo Reyes <angeloreyes@angelos-air.attlocal.net>
2020-04-14 21:22:27 -0700

    fixed bugs

--- scripts/loader.js
+++ scripts/loader.js
@@ -41,3 +41,20 @@ $(window).on("load",function() {
     });
   }).scroll(); 
 });
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".contact-box").each(function() {
+      
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      
+      if (objectBottom < windowBottom) { 
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { 
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); 
+});
