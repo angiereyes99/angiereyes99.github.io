c528b459dc5e6216c62f9fb72362a95c15dee976
Angelo Reyes <angeloreyes1180@yahoo.com>
2020-07-15 13:26:08 -0700

    new desgin

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#52575c"
+        "value": "#28b56c"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#52575c",
+        "color": "#28b56c",
         "opacity": 0.4,
         "width": 1
       },
