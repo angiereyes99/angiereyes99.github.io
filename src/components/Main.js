d8143339c68e2ee96e3275877bd97bba691cf3c8
angiereyes99 <angeloreyes1180@yahoo.com>
2021-12-12 20:20:06 -0800

    converting to react

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import App from '../App';
+
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is a test.</h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
